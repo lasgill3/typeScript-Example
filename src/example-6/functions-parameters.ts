1b8c2b6fb649b1e0bec3cade7ed5b9decb78a3a5
LA-DESKTOP\lasgi <lasgill@gmail.com>
2025-10-15 16:06:06 -0400

    fucntions with parameters

---
+++ src/example-6/functions-parameters.ts
@@ -0,0 +1,25 @@
+//Simple function using typescript
+//traditional function
+function greet(name: string) {
+    return "hello, " + name;
+}   
+
+//call the fucntion with a parameter
+console.log(greet("Sarah"));
+
+//A simple arrow function
+//newer syntax
+const welcome = (name: string ): string => {
+    return "hello, " + name; 
+}   
+
+//call the arrow function with a parameter
+console.log(welcome("John"));
+
+
+const beet =(name: string, age: number) => {
+    return `Hello, ${name}. You are ${age} years old.`; 
+}
+
+console.log(beet("Alice", 30));
+
